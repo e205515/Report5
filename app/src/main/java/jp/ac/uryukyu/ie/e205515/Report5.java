d238804bdbe06d1adc631963475295b56e7b2752
TOMOKI CHINEN <e205515@ie.u-ryukyu.ac.jp>
2021-12-21 22:33:35 +0900

    step3

--- app/src/main/java/jp/ac/uryukyu/ie/e205515/Report5.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205515/Report5.java
@@ -2,12 +2,20 @@ package main.java.jp.ac.uryukyu.ie.e205515;
 
 public class Report5 {
     public static void main(String[] args) {
-        
+    /*
         try {
             String str = null;
             System.out.println(str.length());
         } catch (NullPointerException e) {
             System.out.println("NullPointerExceptionが発生！");
+            System.out.println(e.getMessage());
+        }
+    */  
+        try {
+            String str = "百二十三";
+            int value = Integer.parseInt(str);
+        } catch (NumberFormatException e){
+            System.out.println("NumberFormatExceptionが発生！");
             System.out.println(e.getMessage());
         }
     }
